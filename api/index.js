54692cacc96abbe05e4c3d82f8f3760ead3f4094
pessolatohenrique <pessolatohenrique@gmail.com>
2021-12-25 14:40:56 -0300

    Inicializacao do projeto

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const database = require("./config/database");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("Hello World!!");
+});
+
+app.listen(3000, () => console.log("API rodando!"));
