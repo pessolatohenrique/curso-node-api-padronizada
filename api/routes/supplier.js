011af39e33058b3d31c388c2990f9186adf96c1f
pessolatohenrique <pessolatohenrique@gmail.com>
2021-12-26 18:08:17 -0300

    CRUD de Supplier com apoio do Sequelize

--- api/routes/supplier.js
+++ api/routes/supplier.js
@@ -3,9 +3,32 @@ const model = require("../models/supplier");
 
 const router = Router();
 
-router.route("/suppliers").get(async (req, res) => {
-  const result = await model.findAll();
-  return res.send(result);
-});
+router
+  .route("/suppliers")
+  .get(async (req, res) => {
+    const result = await model.findAll();
+    return res.send(result);
+  })
+  .post(async (req, res) => {
+    const result = await model.create(req.body);
+    return res.send(result);
+  });
 
+router
+  .route("/suppliers/:id")
+  .get(async (req, res) => {
+    const result = await model.findOne({ where: { id: req.params.id } });
+    return res.send(result);
+  })
+  .put(async (req, res) => {
+    await model.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const result = await model.findOne({ where: { id: req.params.id } });
+
+    return res.send(result);
+  });
 module.exports = router;
