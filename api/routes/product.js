5ccca76f9a587056be906cfc70c919bc3857ae72
pessolatohenrique <pessolatohenrique@gmail.com>
2021-12-29 11:06:08 -0300

    CRUD de produtos considerando hierarquia REST

--- api/routes/product.js
+++ api/routes/product.js
@@ -3,14 +3,30 @@ const model = require("../models/product");
 
 const router = Router();
 
-router.route("/supplier/:id/products").get(async (req, res) => {
-  const result = await model.findAll({
-    where: {
-      supplierId: req.params.id,
-    },
+router
+  .route("/supplier/:id/products")
+  .get(async (req, res) => {
+    const result = await model.findAll({
+      where: {
+        supplierId: req.params.id,
+      },
+    });
+
+    return res.status(200).send(result);
+  })
+  .post(async (req, res) => {
+    try {
+      const objectToSave = { ...req.body, supplierId: req.params.id };
+      const result = await model.create(objectToSave);
+      return res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(error);
+    }
   });
 
-  return res.status(200).send(result);
+router.route("/products/:id").delete(async (req, res) => {
+  await model.destroy({ where: { id: req.params.id } });
+  return res.status(204).send();
 });
 
 module.exports = router;
